dad851a34be4bb6e93ea3f7a1856801af3e2bf7a
gimgangsan <gimgansan@gmail.com>
2023-12-31 15:49:08 +0900

    _layerMaskCombined 변수를 통한 버그 수정

--- Zombie Game/Assets/Scripts/PlayerParkour.cs
+++ Zombie Game/Assets/Scripts/PlayerParkour.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerParkour : MonoBehaviour
 {
     float _jumpPower = 8f;
     float _verticalSpeed = 1f;
+    // Solid 레이어와 Platform 레이어 두개를 검사
+    int _layerMaskCombined = (1 << 7) | (1 << 8);
     public Transform _ignoredPlatform;
     public Transform _detectedPlatform;
     Rigidbody2D _rigidbody2d;
@@ -17,7 +20,7 @@ public class PlayerParkour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _detectedPlatform = Physics2D.Raycast(transform.position + Vector3.down , Vector2.down, 0.1f, LayerMask.GetMask("Platform")).transform;
+        _detectedPlatform = Physics2D.Raycast(transform.position + Vector3.down , Vector2.down, 0.1f, _layerMaskCombined).transform;
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody2d.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
@@ -27,13 +30,10 @@ public class PlayerParkour : MonoBehaviour
             Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), true);
             _ignoredPlatform = _detectedPlatform;
         }
-        if(_detectedPlatform != null && _ignoredPlatform != null)
+        if (_detectedPlatform != null && _detectedPlatform != _ignoredPlatform)
         {
-            if(_detectedPlatform != _ignoredPlatform)
-            {
-                Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
-                _ignoredPlatform = null;
-            }
+            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+            _ignoredPlatform = null;
         }
     }
 }
