87c1645e292a060a061bd9a112b8a975e07dcdda
gimgangsan <gimgansan@gmail.com>
2023-12-31 14:54:13 +0900

    아래 화살표 한번만 누르면 다음 플렛폼과 닿을 때 까지 떨어짐

--- Zombie Game/Assets/Scripts/PlayerParkour.cs
+++ Zombie Game/Assets/Scripts/PlayerParkour.cs
@@ -6,7 +6,8 @@ public class PlayerParkour : MonoBehaviour
 {
     float _jumpPower = 8f;
     float _verticalSpeed = 1f;
-
+    public Transform _ignoredPlatform;
+    public Transform _detectedPlatform;
     Rigidbody2D _rigidbody2d;
     private void Awake()
     {
@@ -16,17 +17,23 @@ public class PlayerParkour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        _detectedPlatform = Physics2D.Raycast(transform.position + Vector3.down , Vector2.down, 0.1f, LayerMask.GetMask("Platform")).transform;
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody2d.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
         }
         if(Input.GetKey(KeyCode.DownArrow))
         {
             Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), true);
+            _ignoredPlatform = _detectedPlatform;
         }
-        else
+        if(_detectedPlatform != null && _ignoredPlatform != null)
         {
-            Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+            if(_detectedPlatform != _ignoredPlatform)
+            {
+                Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Platform"), false);
+                _ignoredPlatform = null;
+            }
         }
     }
 }
